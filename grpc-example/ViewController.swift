20d7dbc23df2492fdd070bcfe3a76945a4d2a713
yusuf <yusufkildan@scorpapp.com>
2019-10-09 12:57:40 +0300

    Initial Commit

---
+++ grpc-example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  grpc-example
+//
+//  Created by Scorp on 9.10.2019.
+//  Copyright © 2019 Scorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
